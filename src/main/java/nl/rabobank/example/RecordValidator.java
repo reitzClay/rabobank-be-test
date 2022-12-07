2c5f4983c55c6a04cc53ac6de96d7bcaca8327e5
Clayton <reitzclayton@gmail.com>
2022-12-07 14:52:06 +0100

    validator methods added

--- src/main/java/nl/rabobank/example/RecordValidator.java
+++ src/main/java/nl/rabobank/example/RecordValidator.java
@@ -1,6 +1,11 @@
 package nl.rabobank.example;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -9,16 +14,18 @@ import java.util.List;
  */
 public class RecordValidator
 {
-    public void validate(  ) throws IOException {
+    public void validate( String path, String fileType ) throws IOException {
 
     }
 
-    private List<Record> loadRecordsFromXml(String path) throws IOException {
+    private List<Record> loadRecordsFromXml(String path, String xml) throws IOException {
         //TODO Read value from xml
+        final File initialFile = new File("/Users/claytonreitz/Development/rabobank-be-test/src/test/resources/records.xml");
+        final InputStream targetStream = new FileInputStream(initialFile);
         return null;
     };
 
-    private List<Record> loadRecordsFromCsv(String path) throws IOException {
+    private List<Record> loadRecordsFromCsv(String path, String csv) throws IOException {
         //TODO read from csv
         return null;
     };
--- src/test/java/nl/rabobank/example/RecordValidatorTest.java
+++ src/test/java/nl/rabobank/example/RecordValidatorTest.java
@@ -12,4 +12,5 @@ public class RecordValidatorTest
     public void testValidate() {
 
     }
+
 }
