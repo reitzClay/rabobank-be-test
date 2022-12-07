56f7269ec620fdb95b0eb4d84c93dfd437d92ad3
Clayton <reitzclayton@gmail.com>
2022-12-07 14:15:10 +0100

    1st commit

--- src/main/java/nl/rabobank/example/RecordValidator.java
+++ src/main/java/nl/rabobank/example/RecordValidator.java
@@ -1,6 +1,7 @@
 package nl.rabobank.example;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Hello world!
@@ -11,5 +12,15 @@ public class RecordValidator
     public void validate(  ) throws IOException {
 
     }
+
+    private List<Record> loadRecordsFromXml(String path) throws IOException {
+        //TODO Read value from xml
+        return null;
+    };
+
+    private List<Record> loadRecordsFromCsv(String path) throws IOException {
+        //TODO read from csv
+        return null;
+    };
 
 }
--- src/test/java/nl/rabobank/example/RecordValidatorTest.java
+++ src/test/java/nl/rabobank/example/RecordValidatorTest.java
@@ -1,6 +1,8 @@
 package nl.rabobank.example;
 
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Unit test for simple App.
  */
